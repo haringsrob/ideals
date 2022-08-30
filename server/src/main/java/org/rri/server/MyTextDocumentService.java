b366a2450f99f71aa09c4dd9b2f3c6fef42ede8f
ramazan <rmzs0711@yandex.ru>
2022-08-30 12:40:29 +0300

    rename using rename util

--- server/src/main/java/org/rri/server/MyTextDocumentService.java
+++ server/src/main/java/org/rri/server/MyTextDocumentService.java
@@ -1,7 +1,14 @@
 package org.rri.server;
 
+import com.intellij.codeInsight.TargetElementUtil;
+import com.intellij.openapi.application.ApplicationManager;
 import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.editor.Document;
 import com.intellij.openapi.project.DumbService;
+import com.intellij.openapi.util.Disposer;
+import com.intellij.psi.PsiElement;
+import com.intellij.refactoring.rename.RenameUtil;
+import com.intellij.usageView.UsageInfo;
 import org.eclipse.lsp4j.*;
 import org.eclipse.lsp4j.jsonrpc.messages.Either;
 import org.eclipse.lsp4j.services.TextDocumentService;
@@ -15,11 +22,16 @@ import org.rri.server.references.FindDefinitionCommand;
 import org.rri.server.references.FindTypeDefinitionCommand;
 import org.rri.server.references.FindUsagesCommand;
 import org.rri.server.symbol.DocumentSymbolCommand;
+import org.rri.server.util.EditorUtil;
 import org.rri.server.util.Metrics;
+import org.rri.server.util.MiscUtil;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 
@@ -189,22 +201,79 @@ public class MyTextDocumentService implements TextDocumentService {
 
   @Override
   public CompletableFuture<WorkspaceEdit> rename(RenameParams params) {
+
     return CompletableFuture.supplyAsync(() -> {
-//      try {
-//        final var project = session.getProject();
-//        final var pos = params.getPosition();
-//        final var path = LspPath.fromLspUri(params.getTextDocument().getUri());
-//        final var usages = new FindUsagesCommand(pos).runAsync(project, path).get();
-//        final var file = MiscUtil.resolvePsiFile(project, path);
-//        assert file != null;
-//        final var doc = MiscUtil.getDocument(file);
-//        assert doc != null;
-//        final var old_name = Objects.requireNonNull(file.findElementAt(MiscUtil.positionToOffset(doc, pos))).getText();
-//        final var map = usages.stream().
-//      } catch (ExecutionException | InterruptedException ignore) {}
-//      return null;
-//      return new WorkspaceEdit(List.of(Either.forLeft()))
-      return null;
-    });
+          final var ans = new WorkspaceEdit();
+          final var project = session.getProject();
+          final var pos = params.getPosition();
+          final var path = LspPath.fromLspUri(params.getTextDocument().getUri());
+          ApplicationManager.getApplication().invokeAndWait(
+              () -> {
+                MiscUtil.invokeWithPsiFileInReadAction(project, path, (file) -> {
+                      var x = Disposer.newDisposable();
+                      Document doc = MiscUtil.getDocument(file);
+                      if (doc == null) {
+                        return;
+                      }
+                      var targetElementRef = new AtomicReference<PsiElement>();
+                      try {
+                        EditorUtil.withEditor(x, file, pos, editor -> {
+                          var targetElement = TargetElementUtil.findTargetElement(editor, TargetElementUtil.getInstance().getAllAccepted());
+                          targetElementRef.set(targetElement);
+                        });
+                      } finally {
+                        Disposer.dispose(x);
+                      }
+//                      var renamePsiElementProcessorBase =
+//                          RenamePsiElementProcessorBase.forPsiElement(targetElementRef.get());
+//                      var collection = renamePsiElementProcessorBase.findReferences(
+//                          targetElementRef.get(),
+//                          PsiSearchHelper.getInstance(project).getUseScope(targetElementRef.get()),
+//                          false);
+//                      LOG.warn(collection.toString());
+                      var array = RenameUtil.findUsages(targetElementRef.get(),
+                          params.getNewName(),
+                          false, false, Map.of());
+                      for (UsageInfo usageInfo : array) {
+                        var psiFile = usageInfo.getFile();
+                        assert psiFile != null;
+                        var lspPath = LspPath.fromVirtualFile(psiFile.getVirtualFile());
+                        LOG.warn(usageInfo.getNavigationRange().toString());
+                      }
+                      ans.setChanges(Arrays.stream(array).collect(
+                              Collectors.toMap(usageInfo -> {
+                                    var psiFile = usageInfo.getFile();
+                                    assert psiFile != null;
+                                    var lspPath = LspPath.fromVirtualFile(psiFile.getVirtualFile());
+                                    LOG.warn(usageInfo.getNavigationRange().toString());
+                                    return lspPath.toLspUri();
+                                  },
+                                  usageInfo -> {
+                                    var start = MiscUtil.offsetToPosition(doc,
+                                        usageInfo.getNavigationRange().getStartOffset());
+                                    var end = MiscUtil.offsetToPosition(doc,
+                                        usageInfo.getNavigationRange().getEndOffset());
+                                    return List.of(MiscUtil.with(new TextEdit(), textEdit -> {
+                                      textEdit.setRange(new Range(start, end));
+                                      textEdit.setNewText(params.getNewName());
+                                    }));
+                                  },
+                                  (edits, edits2) -> {
+                                    var merge = new ArrayList<>(edits);
+                                    merge.addAll(edits2);
+                                    return merge;
+                                  }
+                              )
+                          )
+                      );
+//                      LOG.warn(Arrays.toString(array));
+                    }
+                );
+              });
+
+
+          return ans;
+        }
+    );
   }
 }
